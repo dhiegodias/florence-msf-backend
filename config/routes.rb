776c887e807ed0e69e405fe07c7c7a3a94e438d7
Carol <fflorescarol@terra.com.br>
2022-07-02 14:45:27 -0300

    ajustando rotas

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :agendas
+      resources :agendas, only: [:index, :show, :create, :destroy]
     end
   end
 end
