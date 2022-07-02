ba562318f5700673a4304b15273a908e8fdf0236
Carol <fflorescarol@terra.com.br>
2022-07-02 12:05:32 -0300

    criando pasta de seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# import all seeds
+CURRENT_PATH = File.dirname(__FILE__)
+Dir["#{CURRENT_PATH}/seeds/**/*.rb"].sort.each do |file_path|
+  puts "----------------------------------------------"
+  require file_path
+  puts "\r\n"
+end
+puts "----------------------------------------------"
