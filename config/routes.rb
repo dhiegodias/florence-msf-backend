c9611a100e7f7f629898e55ff36fd7e812a8e7fa
Carol <fflorescarol@terra.com.br>
2022-07-02 15:29:57 -0300

    incluindo rota do update

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :agendas, only: [:index, :show, :create, :destroy]
+      resources :agendas, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
