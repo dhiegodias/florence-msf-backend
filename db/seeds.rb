c850edd76ce57b2ebaa14ed66dbdb05b53dcc7a0
Daniele Vasconcelos <vasc.daniele@gmail.com>
2022-07-02 16:22:18 -0300

    schema e seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+news_sources = [
+  {
+    id: 1,
+    name: "G1",
+    url: "www.g1.com.br",
+    credibility: "confiável"
+  },
+
+  {
+    id: 2,
+    name: "R7",
+    url: "www.r7.com",
+    credibility: "Noticia tendenciosa"
+  },
+
+  {
+    id: 3,
+    name: "Senado Federal",
+    url: "https://www12.senado.leg.br/hpsenado",
+    credibility: "Confiável"
+  },
+
+  {
+    id: 4,
+    name: "IBGE",
+    url: "https://www.ibge.gov.br/",
+    credibility: "confiável"
+  },
+]
+
+news_sources.each_with_index do |news_source, index|
+  next if NewsSource.exists?(id: news_source[:id])
+
+  NewsSource.create(
+    id: news_source[:id],
+    name: news_source[:name],
+    url: news_source[:url],
+    credibility: news_source[:credibility]
+  )
+
+  prefix = "Criando fontes de noticias:"
+  prefix = "√ Criando fontes de noticias criadas:" if index == news_sources.count - 1
+
+  print "#{prefix} #{index + 1} de #{news_sources.count} \r"
+end
+
