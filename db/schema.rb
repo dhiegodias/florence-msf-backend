10e03fda9dc2e13c66f6894559992be4bb881400
Dhiego Dias <ddias003@gmail.com>
2022-07-03 16:06:20 -0300

    Adicao de migracao de agenda_id a stakeholders

--- db/schema.rb
+++ db/schema.rb
@@ -1,11 +1,5 @@
-ActiveRecord::Schema[7.0].define(version: 2022_07_02_151835) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_03_184752) do
   enable_extension "plpgsql"
-
-  create_table "news_sources", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.string "credibility"
-  end
 
   create_table "agendas", force: :cascade do |t|
     t.string "title"
@@ -13,7 +7,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_02_151835) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
+  create_table "news_sources", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "credibility"
+  end
+
   create_table "stakeholders", force: :cascade do |t|
     t.string "name", null: false
     t.string "position"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_02_151835) do
     t.string "phone"
     t.string "email"
     t.string "organization"
+    t.integer "agenda_id"
   end
 
 end
