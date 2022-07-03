8bdc87517b522479b50d2de00baf2e6c6e4e80fa
Carol <fflorescarol@terra.com.br>
2022-07-03 11:31:05 -0300

    criando modulo de configuracao para o twitter

---
+++ config/initializers/app_config.rb
@@ -0,0 +1,10 @@
+CONFIG = YAML.load_file(Rails.root.join("config/app.yml"))[Rails.env]
+
+module APP_CONFIG
+  module TWITTER
+    CONSUMER_KEY        = CONFIG["twitter"]["consumer_key"]
+    CONSUMER_SECRET     = CONFIG["twitter"]["consumer_secret"]
+    ACCESS_TOKEN        = CONFIG["twitter"]["access_token"]
+    ACCESS_TOKEN_SECRET = CONFIG["twitter"]["access_token_secret"]
+  end
+end
