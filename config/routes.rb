7a983b948a907382494fe455205b296c7aac540e
Carol <fflorescarol@terra.com.br>
2022-07-03 11:31:26 -0300

    adicionando rota de trending topics

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :agendas, only: [:index, :show, :create, :update, :destroy]
+      get "trending_topics", to: "trending_topics#index"
     end
   end
 end
